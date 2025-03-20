6a81a48f6b95396f9fa0ea7c4c6d6b0ec006a01c
MarlenaMizzoni <165829120+MarlenaMizzoni@users.noreply.github.com>
2025-03-20 09:06:50 +0000

    Input JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,78 @@
-console.log('OK');
+gsap.to('#GOOGLE' , {
+  
+    fill: '#FF5F1F',
+    duration: 9,
+    repeat: -1,
+    yoyo: true,
+  });
+  
+  gsap.to('.SKY' , {
+    
+    fill: '#e4f3ff',
+    duration: 9, 
+    repeat: -1,
+    yoyo: true, 
+   
+    
+  });
+  
+  
+  
+  
+  gsap.to('#SUN' , {
+    
+    y: -100,
+    duration: 9,
+    repeat: -1,
+    yoyo: true,
+    
+    
+  });
+  
+  gsap.to('.WINDOWS2' , {
+    
+    duration: 10, 
+    repeat: -1,
+    fill: "Khaki" ,
+    stagger: .5 , 
+    ease: 'elastic',
+    yoyo: true,
+    
+  });
+  
+  
+  gsap.to('.WINDOWS1' , {
+    
+    duration: 2, 
+    repeat: -1,
+    fill: "Khaki" ,
+    stagger: 2 , 
+    ease: 'elastic',
+    yoyo: true,
+  });
+  
+  gsap.to('.LAMPLIGHT1' , {
+    fill: "yellow",
+  duration: 1,
+    repeat: -1,
+    
+    
+  });
+  
+  
+  gsap.to('.LAMPLIGHT2' , {
+    fill: "yellow",
+  duration: 2,
+    repeat: -1,
+    
+    
+  });
+  
+  gsap.to('.LAMPLIGHT3' , {
+    fill: "yellow",
+  duration: 3,
+    repeat: -1,
+   
+    
+  });
+
